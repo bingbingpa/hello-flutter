cde77a244e14d1c4a4735c3adeaf0b7f223bcffd
shpark <bingbingpa@gmail.com>
2024-10-06 23:01:43 +0900

    [row and column] 위젯 배치 실습

--- row_and_column/lib/screen/home_screen.dart
+++ row_and_column/lib/screen/home_screen.dart
@@ -8,18 +8,90 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          color: Colors.black,
-          child: Column(
-            children: colors
-                .map((e) => Container(
-                      height: 50.0,
-                      width: 50.0,
-                      color: e,
-                    ))
-                .toList(),
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: colors.map((e) => Container(
+                height: 50.0,
+                width: 50.0,
+                color: e,
+              )).toList(),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 50.0,
+                  width: 50.0,
+                  color: Colors.orange,
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: colors.map((e) => Container(
+                height: 50.0,
+                width: 50.0,
+                color: e,
+              )).toList(),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 50.0,
+                  width: 50.0,
+                  color: Colors.green,
+                )
+              ],
+            )
+          ],
         ),
+        // child: Container(
+          // color: Colors.black,
+          // width: double.infinity,
+          // height: double.infinity,
+          // child: Row(
+          // child: Column(
+            // mainAxisAlignment: MainAxisAlignment.start,
+            // mainAxisSize: MainAxisSize.max,
+            // crossAxisAlignment: CrossAxisAlignment.stretch,
+            // children: colors
+            //     .map((e) => Container(
+            //           height: 50.0,
+            //           width: 50.0,
+            //           color: e,
+            //         ))
+            //     .toList(),
+            // children: [
+            //   Flexible(
+            //     flex: 2,
+            //     fit: FlexFit.tight,
+            //     child: Container(
+            //       height: 50.0,
+            //       width: 50.0,
+            //       color: Colors.red,
+            //     ),
+            //   ),
+            //   Expanded(
+            //     child: Container(
+            //       height: 50.0,
+            //       width: 50.0,
+            //       color: Colors.orange,
+            //     ),
+            //   ),
+            //   Expanded(
+            //     child: Container(
+            //       height: 50.0,
+            //       width: 50.0,
+            //       color: Colors.yellow,
+            //     ),
+            //   )
+            // ],
+          // ),
+        // ),
       ),
     );
   }
