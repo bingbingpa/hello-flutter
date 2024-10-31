2d65cbf3d421169cf5b63c0602867e3085998270
shpark <bingbingpa@gmail.com>
2024-10-31 22:35:35 +0900

    [Navigator] Declarative Routing 과 Named Routes

--- navigation/lib/screen/route_two_screen.dart
+++ navigation/lib/screen/route_two_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:navigation/layout/default_layout.dart';
+import 'package:navigation/screen/route_three_screen.dart';
 
 class RouteTwoScreen extends StatelessWidget {
   const RouteTwoScreen({super.key});
@@ -20,6 +21,15 @@ class RouteTwoScreen extends StatelessWidget {
             Navigator.of(context).pop();
           },
           child: Text('Pop'),
+        ),
+        OutlinedButton(
+          onPressed: () {
+            Navigator.of(context).pushNamed(
+              '/three',
+              arguments: 11111,
+            );
+          },
+          child: Text('Push Route Three'),
         ),
       ],
     );
